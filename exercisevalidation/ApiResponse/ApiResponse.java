c04ec43b7c805aab449866162a94ad21b34f1dd8
Hadeel-aldawsari <hadeel.amg99@gmail.com>
2024-11-18 14:53:20 +0300

    Add files via upload

---
+++ exercisevalidation/ApiResponse/ApiResponse.java
@@ -0,0 +1,10 @@
+package projectSysytem.exercisevalidation.ApiResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private String messege;
+}
